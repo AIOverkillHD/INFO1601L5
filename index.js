55ad01b157853cabe210895e7862c114df66b6b1
OHD <168602852+AIOverkillHD@users.noreply.github.com>
2026-03-09 00:21:42 -0400

    Lab 5

--- index.js
+++ index.js
@@ -1,5 +1,176 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
-
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
+/* //task 5.2
+let arr = [12, 33, 4, 5, -4, 8, 19, 25];
+                
+//map() creates a new array from the elements of one without changing the old one
+function double(num){
+  return num * 2;
 }
+
+let doubledArr = arr.map(double); 
+console.log(doubledArr);
+
+function isOdd(num){
+  return num%2 !== 0; 
+}
+//Filter takes a test condition and returns only the element which 
+//make the condition true
+let odds = arr.filter(isOdd);
+console.log(odds);
+
+//Returns true or false if any of the elements of the array 
+//meets a specified condition
+function has5Factor(num){
+  return num % 5 === 0;
+}
+
+let hasFiveFactor = arr.some(has5Factor);
+console.log(hasFiveFactor);
+
+function intCompare(a, b){
+ return a - b;
+}
+
+//sort function must return either 0, +ve, -ve
+let ascending = arr.sort(intCompare);
+console.log(ascending); */
+
+
+/* //task 5.1
+let arr = [ -5, 16, 33, 42, 103, 344];
+
+console.log(arr.includes(-5));//true
+
+arr.push(11);//adds item to the end
+
+console.log(arr)//[ -5, 16, 33, 42, 103, 344, 11];
+
+let lastItem = arr.pop();//removes last item
+
+console.log(lastItem, arr);//11, [ -5, 16, 33, 42, 103, 344]
+
+arr.unshift(22);//adds item to the front
+
+console.log(arr);//[22, -5, 16, 33, 42, 103, 344]
+
+let firstItem = arr.shift();//removes first item
+console.log(firstItem, arr);//22, [-5, 16, 33, 42, 103, 344]
+
+let reversed = arr.reverse();//creates a new array in reverse order 
+console.log(reversed);//[344, 103, 42, 33, 16, -5]
+console.log(arr.join('-'));//"-5-103-16-33-344-42" joins array with provided separator */
+
+
+
+/* //task 2
+//prints current date
+function printDate(){
+   console.log(new Date().toLocaleTimeString());
+}
+      
+setInterval(printDate, 1000); */
+
+
+/* //task 5
+function happyPrint(string){
+ console.log("😀: "+string);
+}
+
+function sadPrint(string){
+ console.log("😢: "+string);
+}
+
+
+function sendEmailToCustomer(items, email){
+  console.log("Email sent to "+email+ "with the following items: "+items);
+}
+
+//This high-order function adds 2 parameters and passes the answer to the callback
+function orderFood(item1, item2, email, sendEmail){
+  let arr = [item1, item2];
+  
+  let total = 0;
+  let itemSold = ["Pizza", "Soda", "Burger"];
+  let isItemPrices = [10, 2, 8];
+
+  for(let i=0; i<arr.length; i++){
+    let index = itemSold.indexOf(arr[i]);
+    if(index !== -1){
+      total += isItemPrices[index];
+    }
+  }
+
+  sendEmail(arr, total, email);
+}
+
+orderFood("Pizza", "Soda", "e.xample@gmail.com", sendEmailToCustomer);
+ */
+
+
+/* //task 4.3
+for(let i=0; i< 50; i++){
+  if(i %3 === 0 && i%5 === 0){ // is divisible by 3 and 5?
+    console.log('fizzbuzz');
+  }else if(i%3 === 0){ // ok it's not divisible by both how about just 3?
+    console.log('fizz');
+  }else if(i%5 === 0){ //no? what about just 5 then?
+    console.log('buzz');
+  }else{              //so its not divisible by 3 or 5 then
+    console.log(i);
+  }
+} */
+
+
+/* let nowTimestamp = Date.now();//store the current Date
+
+console.log(nowTimestamp);//unix timestamp in milliseconds 1581291269848
+
+let now = new Date(nowTimestamp);//today's Date object 
+
+//months go from 0 - 11
+let date = new Date(2019, 11, 17, 3, 24, 0);//December 17, 2019 03:24:00
+//printing the date
+
+console.log(date.toLocaleDateString("en-US"));//12/17/2019
+//1 is added to month because January is 0
+console.log(` ${now.getDate()} - ${now.getMonth() + 1} - ${now.getFullYear()} `); // 9 - 2 - 2020 
+
+//Calculate difference between two dates in milliseconds
+let difference = now - date;
+//convert milliseconds to days
+
+let daysBetween = difference * 1.15741e-8;
+console.log(daysBetween);//54.6740790437368 */
+
+
+
+
+
+/* //task 6
+//to go over next lab
+//Create a constructor a functions which builds object for us
+function createPerson(name, height, weight) {
+  return { name: name, height: height, weight: weight };
+}
+
+function calcBMI(weight, height) {
+  return weight / (height * height);
+}
+
+function avgBMI(people) {
+  let sum = 0;
+  for (let person of people) {
+    //sum the bmi of each person
+    sum += calcBMI(person.weight, person.height);
+  }
+  //calculate average
+  return sum / people.length;
+}
+
+//create a collection of people
+let people = [
+  createPerson("Sally", 60, 2.5),
+  createPerson("Ben", 81, 3),
+  createPerson("Shelly", 50, 1.7)
+];
+
+console.log(avgBMI(people)); */
